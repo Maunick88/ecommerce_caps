a9ceb768536d7e116c7337cc86cb95ca3a902369
Mauricio Nicolas <mauricio@codeman.company>
2024-09-06 18:03:32 -0600

    footer

--- resources/views/teams/mlb/dodgers.blade.php
+++ resources/views/teams/mlb/dodgers.blade.php
@@ -1,4 +1,4 @@
-@include('header')  
+@include('header')   
     <body>
     <section>
         <div class="container">
@@ -91,51 +91,8 @@
 
 
     </section>
-    <section class="footer-section">
-        <div class="line"></div>
+        @include('footer')   
 
-        <div class="container sides">
-            <div class="left-side">
-                <div class="logo">
-                    <img src="./img/star.png" class="star" alt="">
-                    <div>Frost Inv.</div>
-                </div>
-
-                <p class="p">
-                    We are based in Los Angeles, USA.<br>
-                    our motto is -investing in knowledge <br>
-                    pays the best dividends.
-                </p>
-            </div>
-            <div class="right-side">
-                <ul class="menu">
-                    <li><a href="#">Resources</a></li>
-                    <li><a href="#">company</a></li>
-                    <li><a href="#">help</a></li>
-                    <li><a href="#">client</a></li>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">support</a></li>
-                    <li><a href="#">blog</a></li>
-                    <li><a href="#">services</a></li>
-                    <li><a href="#">jobs</a></li>
-                    <li><a href="#">docs</a></li>
-                    <li><a href="#">news</a></li>
-                    <li><a href="#">privacy policy</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
-
-    <footer>
-        <div class="container">
-            <div class="left-side">
-                <p> &copy; copyright 2022 . All rights reserved</p>
-            </div>
-            <div class="right-side center">
-                <p>Term and Conditions</p>
-            </div>
-        </div>
-    </footer>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js" ></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -560,68 +560,8 @@
         </div>
     </section>
 
-    <section class="footer-section">
-        <div class="container" style="padding-bottom:5rem;">
-            <img src="./img/star.png" alt="" class="star star-s5">
-            <div class="left-side">
-                <div class="title">
-                    <h1>stay up-to date</h1>
-                    <h2>With everything precious metals</h2>
-                </div>
-            </div>
-            <div class="right-side">
-                <p class="txt">
-                    Replenish <span>your portfolio</span>
-                    with gold and silver <span>to diversity</span>
-                    your investment portfolio.
-                </p>
-            </div>
-        </div>
+    @include('footer')   
 
-        <div class="line"></div>
-
-        <div class="container sides">
-            <div class="left-side">
-                <div class="logo">
-                    <img src="./img/star.png" class="star" alt="">
-                    <div>Frost Inv.</div>
-                </div>
-
-                <p class="p">
-                    We are based in Los Angeles, USA.<br>
-                    our motto is -investing in knowledge <br>
-                    pays the best dividends.
-                </p>
-            </div>
-            <div class="right-side">
-                <ul class="menu">
-                    <li><a href="#">Resources</a></li>
-                    <li><a href="#">company</a></li>
-                    <li><a href="#">help</a></li>
-                    <li><a href="#">client</a></li>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">support</a></li>
-                    <li><a href="#">blog</a></li>
-                    <li><a href="#">services</a></li>
-                    <li><a href="#">jobs</a></li>
-                    <li><a href="#">docs</a></li>
-                    <li><a href="#">news</a></li>
-                    <li><a href="#">privacy policy</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
-
-    <footer>
-        <div class="container">
-            <div class="left-side">
-                <p> &copy; copyright 2022 . All rights reserved</p>
-            </div>
-            <div class="right-side center">
-                <p>Term and Conditions</p>
-            </div>
-        </div>
-    </footer>
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/gsap.min.js" ></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.10.4/ScrollTrigger.min.js" ></script>
